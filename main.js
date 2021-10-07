5ffab76a91931490b4d190cfa53ce0de6da76f06
Laura-Hassert <laura.hassert@gmail.com>
2021-10-07 16:39:11 -0400

    playing with styles/functions

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ document.querySelector('#button').addEventListener("click", () => {
     window.open("https://www.ma-ka-rohn.com/collections/best-macaron-flavors")
 });
 
+document.querySelector('#button').addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = "black"
+    event.target.style.color = "lightsalmon"
+});
+
+document.querySelector('#button').addEventListener('mouseoff', (event) => {
+    event.target.style.backgroundColor = "lightsalmon"
+    event.target.style.color = "black"
+});
+
 document.querySelector('#image').addEventListener('mouseover', () => {
     alert('OMG those look SO good!')
 })
