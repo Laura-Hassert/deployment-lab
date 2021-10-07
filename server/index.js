13979f04c9d23bc4599ca75744f8167c2f29c19d
Laura-Hassert <laura.hassert@gmail.com>
2021-10-07 14:42:14 -0400

    init commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+  })
+
+const port = process.env.PORT || 4545;
+app.listen(port, () => {console.log(`Server up on port ${port}`)});
