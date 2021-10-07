6218599afef1fd516019b42a09dbaea2417f8092
Laura-Hassert <laura.hassert@gmail.com>
2021-10-07 14:57:14 -0400

    take four

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use('/js', express.static(path.join(__dirname, 'index.html')))
-
-app.get('/js', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
   })
 
