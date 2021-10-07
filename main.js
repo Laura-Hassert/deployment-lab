789621e714bd4a1a2b1957103c89f76e330cdd01
Laura-Hassert <laura.hassert@gmail.com>
2021-10-07 16:17:20 -0400

    hoping css works on the 5th try

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('#button').addEventListener("click", () => {
+    window.open("https://www.ma-ka-rohn.com/collections/best-macaron-flavors")
+});
+
+document.querySelector('#image').addEventListener('mouseover', () => {
+    alert('OMG those look SO good!')
+})
